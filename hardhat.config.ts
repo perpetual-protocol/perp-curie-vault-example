73bde750e83856a6c6e63fc857608b6843a806c0
Wraecca <wraecca@gmail.com>
2022-07-09 16:55:12 +0800

    add config files

---
+++ hardhat.config.ts
@@ -0,0 +1,59 @@
+import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-waffle"
+import "@openzeppelin/hardhat-upgrades"
+import "@typechain/hardhat"
+import "hardhat-contract-sizer"
+import "hardhat-dependency-compiler"
+import "hardhat-gas-reporter"
+import { HardhatUserConfig } from "hardhat/config"
+import "solidity-coverage"
+import "./mocha-test"
+
+const config: HardhatUserConfig = {
+    solidity: {
+        version: "0.7.6",
+        settings: {
+            optimizer: { enabled: true, runs: 100 },
+            evmVersion: "berlin",
+            // for smock to mock contracts
+            outputSelection: {
+                "*": {
+                    "*": ["storageLayout"],
+                },
+            },
+        },
+    },
+    networks: {
+        hardhat: {
+            allowUnlimitedContractSize: true,
+        },
+    },
+    dependencyCompiler: {
+        // We have to compile from source since UniswapV3 doesn't provide artifacts in their npm package
+        paths: [
+            "@uniswap/v3-core/contracts/UniswapV3Factory.sol",
+            "@uniswap/v3-core/contracts/UniswapV3Pool.sol",
+            "@perp/perp-oracle-contract/contracts/ChainlinkPriceFeedV2.sol",
+            "@perp/perp-oracle-contract/contracts/BandPriceFeed.sol",
+            "@perp/perp-oracle-contract/contracts/EmergencyPriceFeed.sol",
+        ],
+    },
+    contractSizer: {
+        // max bytecode size is 24.576 KB
+        alphaSort: true,
+        runOnCompile: true,
+        disambiguatePaths: true,
+        except: ["@openzeppelin/", "@uniswap/", "@perp/perp-oracle-contract/", "test/"],
+    },
+    gasReporter: {
+        excludeContracts: ["test"],
+    },
+    mocha: {
+        require: ["ts-node/register/files"],
+        jobs: 4,
+        timeout: 120000,
+        color: true,
+    },
+}
+
+export default config
