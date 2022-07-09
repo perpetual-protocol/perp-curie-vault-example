82d5e2b964d56a78620a0434c13c813ed82231ba
Wraecca <wraecca@gmail.com>
2022-07-09 16:56:18 +0800

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,13 +7,12 @@ import "hardhat-dependency-compiler"
 import "hardhat-gas-reporter"
 import { HardhatUserConfig } from "hardhat/config"
 import "solidity-coverage"
-import "./mocha-test"
 
 const config: HardhatUserConfig = {
     solidity: {
         version: "0.7.6",
         settings: {
-            optimizer: { enabled: true, runs: 100 },
+            optimizer: { enabled: true, runs: 200 },
             evmVersion: "berlin",
             // for smock to mock contracts
             outputSelection: {
@@ -28,22 +27,12 @@ const config: HardhatUserConfig = {
             allowUnlimitedContractSize: true,
         },
     },
-    dependencyCompiler: {
-        // We have to compile from source since UniswapV3 doesn't provide artifacts in their npm package
-        paths: [
-            "@uniswap/v3-core/contracts/UniswapV3Factory.sol",
-            "@uniswap/v3-core/contracts/UniswapV3Pool.sol",
-            "@perp/perp-oracle-contract/contracts/ChainlinkPriceFeedV2.sol",
-            "@perp/perp-oracle-contract/contracts/BandPriceFeed.sol",
-            "@perp/perp-oracle-contract/contracts/EmergencyPriceFeed.sol",
-        ],
-    },
     contractSizer: {
         // max bytecode size is 24.576 KB
         alphaSort: true,
         runOnCompile: true,
         disambiguatePaths: true,
-        except: ["@openzeppelin/", "@uniswap/", "@perp/perp-oracle-contract/", "test/"],
+        except: ["@openzeppelin/", "@uniswap/", "@perp/", "test/"],
     },
     gasReporter: {
         excludeContracts: ["test"],
